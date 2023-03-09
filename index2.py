9deb7f4a0e311f0fec8c146d4c0085e52a52887b
luizh3nrique <luizh3nrique00@gmail.com>
2023-03-08 23:23:52 -0300

    Fix - Modificaçao nos aquivos

---
+++ index2.py
@@ -0,0 +1,5 @@
+n1=float(input('primeira nota do aluno: '))
+n2=float(input('segunda nota do aluno: '))
+n3=float(input('a terceira nota do aluno: '))
+m=(n1+n2+n3)/3
+print('a media entre {} e {} e {} � igual a {:.2f}'.format(n1,n2,n3,m))
---
+++ index3.py
@@ -0,0 +1,3 @@
+real=float(input('quanto dinheiro voce tem na carteira? R$ '))
+dolar = real / 5.30
+print('com R${:.2f} voce pode comprar ${:.2f} dolares'.format(real, dolar))
