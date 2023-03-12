591f775b56f2013da6e13096b3a18b2c9dcb623b
Luiz Henrique <126086100+Luizh3nr1que@users.noreply.github.com>
2023-03-12 14:56:30 -0300

    Add files via upload

---
+++ index5.py
@@ -0,0 +1,6 @@
+n=int(input('digite um valor: '))
+d=n*2
+t=n*3
+r=n**(1/2)
+print('o dobro de {} vale {}'.format(n,d))
+print('o triplo de {} vale {}.A raiz quadrada de {} e igual a {:.2f}'.format(n, t, n, r))
